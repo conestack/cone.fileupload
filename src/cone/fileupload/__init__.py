91c80ba7150e2cb0180e622d5e27bb1e49631b5d
Robert Niederreiter <office@squarewave.at>
2015-02-14 17:45:09 +0100

    resources delivery and tempate view wip

--- src/cone/fileupload/__init__.py
+++ src/cone/fileupload/__init__.py
@@ -7,15 +7,35 @@ logger = logging.getLogger('cone.fileupload')
 
 
 # protected CSS
-#cone.app.cfg.css.protected.append(
-#    'fileupload-static/jquery-fileupload/css/jquery.fileupload-ui.css')
+css = cone.app.cfg.css
+css.protected.append('fileupload-static/css/jquery.fileupload-ui.css')
+
+
+# protected JS
+js = cone.app.cfg.js
+js.protected.append('fileupload-static/js/vendor/tmpl.min.js')
+js.protected.append('fileupload-static/js/vendor/load-image.all.min.js')
+js.protected.append('fileupload-static/js/vendor/canvas-to-blob.min.js')
+js.protected.append('fileupload-static/js/vendor/jquery.blueimp-gallery.min.js')
+js.protected.append('fileupload-static/js/jquery.iframe-transport.js')
+js.protected.append('fileupload-static/js/jquery.fileupload.js')
+js.protected.append('fileupload-static/js/jquery.fileupload-process.js')
+js.protected.append('fileupload-static/js/jquery.fileupload-image.js')
+js.protected.append('fileupload-static/js/jquery.fileupload-audio.js')
+js.protected.append('fileupload-static/js/jquery.fileupload-video.js')
+js.protected.append('fileupload-static/js/jquery.fileupload-validate.js')
+js.protected.append('fileupload-static/js/jquery.fileupload-ui.js')
+js.protected.append('fileupload-static/js/main.js')
 
 
 # application startup initialization
 def initialize_fileupload(config, global_config, local_config):
     # static resources
-    config.add_view('cone.fileupload.browser.static_resources',
-                    name='fileupload-static')
+    config.add_view(
+        'cone.fileupload.browser.static_resources',
+        name='fileupload-static'
+    )
+
     # scan browser package
     config.scan('cone.fileupload.browser')
 
--- src/cone/fileupload/browser/fileupload.py
+++ src/cone/fileupload/browser/fileupload.py
@@ -1,37 +1,37 @@
 from pyramid.view import view_config
 from pyramid.i18n import TranslationStringFactory
 from cone.tile import tile
+from cone.app.browser import render_main_template
 from cone.app.browser.actions import LinkAction
 from cone.app.browser.contextmenu import context_menu
 from cone.app.browser.layout import ProtectedContentTile
 from cone.app.browser.utils import make_url
-from ..model import Slide
+
 
 _ = TranslationStringFactory('cone.fileupload')
 
 
-class FileUploadAction(LinkAction):
-    id = 'toolbaraction-fileupload'
-    css = ''
-    icon = 'toolbaricon-fileupload'
-    title = _('fileupload', default=u'File Upload')
-    action = 'fileupload:#content:inner'
-    href = LinkAction.target
+# class FileUploadAction(LinkAction):
+#     id = 'toolbaraction-fileupload'
+#     css = ''
+#     icon = 'toolbaricon-fileupload'
+#     title = _('fileupload', default=u'File Upload')
+#     action = 'fileupload:#content:inner'
+#     href = LinkAction.target
+# 
+#     @property
+#     def display(self):
+#         return isinstance(self.model, Slide)
+# 
+#     @property
+#     def selected(self):
+#         return self.action_scope == 'fileupload'
+# 
+# 
+# context_menu['contentviews']['fileupload'] = FileUploadAction(True)
 
-    @property
-    def display(self):
-        return isinstance(self.model, Slide)
 
-    @property
-    def selected(self):
-        return self.action_scope == 'fileupload'
-
-
-#context_menu['contentviews']['fileupload'] = FileUploadAction(True)
-
-
-@tile('fileupload', 'templates/fileupload.pt',
-      interface=Slide, permission='view')
+@tile('fileupload', 'fileupload.pt', permission='add')
 class FileUploadTile(ProtectedContentTile):
 
     @property
@@ -41,6 +41,11 @@ class FileUploadTile(ProtectedContentTile):
                         resource='fileupload_handler')
 
 
-@view_config(name='fileupload_handler', context=Slide, permission='view')
+@view_config('fileupload', permission='add')
+def fileupload(model, request):
+    return render_main_template(model, request, 'fileupload')
+
+
+@view_config(name='fileupload_handler', permission='add')
 def fileupload_handler(model, request):
     pass
