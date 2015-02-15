ce2f773ebe111702ff1354dbd4498f485ec2bfca
Robert Niederreiter <office@squarewave.at>
2015-02-15 20:51:29 +0100

    use str.format for upload and download templates

--- src/cone/fileupload/browser/fileupload.py
+++ src/cone/fileupload/browser/fileupload.py
@@ -13,13 +13,13 @@ _ = TranslationStringFactory('cone.fileupload')
 
 UPLOAD_TEMPLATE = """
 <script id="template-upload" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
+{{% for (var i=0, file; file=o.files[i]; i++) {{ %}}
     <tr class="template-upload fade">
         <td>
             <span class="preview"></span>
         </td>
         <td>
-            <p class="name">{%=file.name%}</p>
+            <p class="name">{{%=file.name%}}</p>
             <strong class="error text-danger"></strong>
         </td>
         <td>
@@ -27,67 +27,67 @@ UPLOAD_TEMPLATE = """
             <div class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="progress-bar progress-bar-success" style="width:0%;"></div></div>
         </td>
         <td>
-            {% if (!i && !o.options.autoUpload) { %}
+            {{% if (!i && !o.options.autoUpload) {{ %}}
                 <button class="btn btn-primary start" disabled>
                     <i class="glyphicon glyphicon-upload"></i>
                     <span>Start</span>
                 </button>
-            {% } %}
-            {% if (!i) { %}
+            {{% }} %}}
+            {{% if (!i) {{ %}}
                 <button class="btn btn-warning cancel">
                     <i class="glyphicon glyphicon-ban-circle"></i>
                     <span>Cancel</span>
                 </button>
-            {% } %}
+            {{% }} %}}
         </td>
     </tr>
-{% } %}
+{{% }} %}}
 </script>
 """
 
 
 DOWNLOAD_TEMPLATE = """
 <script id="template-download" type="text/x-tmpl">
-{% for (var i=0, file; file=o.files[i]; i++) { %}
+{{% for (var i=0, file; file=o.files[i]; i++) {{ %}}
     <tr class="template-download fade">
         <td>
             <span class="preview">
-                {% if (file.thumbnailUrl) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" data-gallery><img src="{%=file.thumbnailUrl%}"></a>
-                {% } %}
+                {{% if (file.thumbnailUrl) {{ %}}
+                    <a href="{{%=file.url%}}" title="{{%=file.name%}}" download="{{%=file.name%}}" data-gallery><img src="{{%=file.thumbnailUrl%}}"></a>
+                {{% }} %}}
             </span>
         </td>
         <td>
             <p class="name">
-                {% if (file.url) { %}
-                    <a href="{%=file.url%}" title="{%=file.name%}" download="{%=file.name%}" {%=file.thumbnailUrl?'data-gallery':''%}>{%=file.name%}</a>
-                {% } else { %}
-                    <span>{%=file.name%}</span>
-                {% } %}
+                {{% if (file.url) {{ %}}
+                    <a href="{{%=file.url%}}" title="{{%=file.name%}}" download="{{%=file.name%}}" {{%=file.thumbnailUrl?'data-gallery':''%}}>{{%=file.name%}}</a>
+                {{% }} else {{ %}}
+                    <span>{{%=file.name%}}</span>
+                {{% }} %}}
             </p>
-            {% if (file.error) { %}
-                <div><span class="label label-danger">Error</span> {%=file.error%}</div>
-            {% } %}
+            {{% if (file.error) {{ %}}
+                <div><span class="label label-danger">Error</span> {{%=file.error%}}</div>
+            {{% }} %}}
         </td>
         <td>
-            <span class="size">{%=o.formatFileSize(file.size)%}</span>
+            <span class="size">{{%=o.formatFileSize(file.size)%}}</span>
         </td>
         <td>
-            {% if (file.deleteUrl) { %}
-                <button class="btn btn-danger delete" data-type="{%=file.deleteType%}" data-url="{%=file.deleteUrl%}"{% if (file.deleteWithCredentials) { %} data-xhr-fields='{"withCredentials":true}'{% } %}>
+            {{% if (file.deleteUrl) {{ %}}
+                <button class="btn btn-danger delete" data-type="{{%=file.deleteType%}}" data-url="{{%=file.deleteUrl%}}"{{% if (file.deleteWithCredentials) {{ %}} data-xhr-fields='{{"withCredentials":true}}'{{% }} %}}>
                     <i class="glyphicon glyphicon-trash"></i>
                     <span>Delete</span>
                 </button>
                 <input type="checkbox" name="delete" value="1" class="toggle">
-            {% } else { %}
+            {{% }} else {{ %}}
                 <button class="btn btn-warning cancel">
                     <i class="glyphicon glyphicon-ban-circle"></i>
                     <span>Cancel</span>
                 </button>
-            {% } %}
+            {{% }} %}}
         </td>
     </tr>
-{% } %}
+{{% }} %}}
 </script>
 """
 
@@ -97,12 +97,18 @@ class FileUploadTile(Tile):
     """Register this tile for specific context if jQuery file upload settings
     should be customized.
     """
-    upload_template = UPLOAD_TEMPLATE
-    download_template = DOWNLOAD_TEMPLATE
     accept_file_types = '' # e.g. /(\.|\/)(gif|jpe?g|png)$/i
     disable_image_preview = False
     disable_video_preview = False
     disable_audio_preview = False
+
+    @property
+    def upload_template(self):
+        return UPLOAD_TEMPLATE.format()
+
+    @property
+    def download_template(self):
+        return DOWNLOAD_TEMPLATE.format()
 
     @property
     def upload_url(self):
